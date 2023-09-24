1bcd650b8ba92e9d105fdaba076f397bf2bf88d1
your_name <smokrenko79@gmail.com>
2023-09-25 00:10:58 +0300

    added create-date.test

---
+++ src/utils/date/test/create-date.test.ts
@@ -0,0 +1,29 @@
+import { describe, expect, test } from "@jest/globals";
+import { createDate } from "../create-date";
+
+describe('create date', () => {
+    test('get date params', () => {
+        const date = new Date('2023-02-01');
+        const params = {
+            locale: 'default',
+            date: date,
+        }
+        const tzOffset = new Date().getTimezoneOffset() * 60000;
+
+        expect(createDate(params)).toBe({
+            date: Date.parse('2023-02-01T03:00:00.000Z'),
+            dayNumber: 1,
+            day: 'среда',
+            dayNumberInWeek: 4,
+            dayShort: 'ср',
+            year: 2023,
+            yearShort: '23',
+            month: 'февраль',
+            monthShort: 'февр.',
+            monthNumber: 2,
+            monthIndex: 1,
+            timestamp: 1675220400000,
+            week: 5,
+        })
+    })
+})
