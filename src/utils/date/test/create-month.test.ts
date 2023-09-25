3c6cf461856747fc4850ce8344ec243999047604
your_name <smokrenko79@gmail.com>
2023-09-25 21:30:54 +0300

    added create-month

--- src/utils/date/test/create-month.test.ts
+++ src/utils/date/test/create-month.test.ts
@@ -1,5 +1,8 @@
-import { describe, expect, test } from "@jest/globals";
-import { createMonth } from "../create-month";
+import { describe, expect, jest, test } from '@jest/globals';
+
+import { createMonth } from '../create-month';
+import { createDate } from '../create-date';
+import { getMonthNumberOfDays } from '../get-month-number-of-days';
 
 describe('create month', () => {
     test('get month params', () => {
@@ -7,10 +10,28 @@ describe('create month', () => {
         const params = {
             locale: 'default',
             date: date,
-        }
+        };
+
+        const getDay = (dayNumber = 1) =>
+            createDate({ date: new Date(2023, 1, dayNumber), locale: 'default' });
+
+        const createMonthDays = () => {
+            const days = [];
+
+            for (let i = 0; i <= getMonthNumberOfDays(1, 2023) - 1; i += 1) {
+                days[i] = getDay(i + 1);
+            }
+
+            return days;
+        };
 
         expect(createMonth(params)).toEqual({
-            
-        })
-    })
-})
+            getDay,
+            monthName: 'февраль',
+            monthIndex: 1,
+            monthNumber: 2,
+            year: 2023,
+            createMonthDays,
+        });
+    });
+});
