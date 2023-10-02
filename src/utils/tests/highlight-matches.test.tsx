ccaea97187352f33f672fdb002078247c45b399f
your_name <smokrenko79@gmail.com>
2023-10-02 21:14:20 +0300

    added highlight-matches

--- src/utils/tests/highlight-matches.test.tsx
+++ src/utils/tests/highlight-matches.test.tsx
@@ -1,25 +1,34 @@
-import { describe, it } from "@jest/globals";
+/**
+ * @jest-environment jsdom
+ */
+import React from 'react';
+import { describe, it, jest } from '@jest/globals';
 import renderer from 'react-test-renderer';
-import { highlightMatches } from "../highlight-matches";
-import React from "react";
+
+import { highlightMatches } from '../highlight-matches';
 
 describe('highlight matches', () => {
     it('returns str', () => {
         expect(highlightMatches('', 'хулиномика')).toBe('хулиномика');
-    })
+    });
+
+    it('has not matches', () => {
+        expect(highlightMatches('хули', '')).toBe('');
+    });
 
     it('searches matches', () => {
-        const substr = "номика";
+        const substr = 'номика';
         const component = renderer
             .create(
-                <React.Fragment key={substr}>
-                    {substr}
+                <React.Fragment>
                     <span style={{ color: '#FF5253' }} data-test-id='highlight-matches'>
                         хули
                     </span>
-                </React.Fragment>
-            ).toJSON();
+                    {substr}
+                </React.Fragment>,
+            )
+            .toJSON();
 
-        expect(component).toMatchSnapshot();
+        expect(highlightMatches('хули', 'хулиномика')).toEqual(component);
     });
 });
